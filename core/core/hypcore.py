90c98b7872ecd0caff82957a830b95dba88ab0cd
cxl2020MC <andamu2019@163.com>
2025-01-08 10:07:01 +0800

    test

--- core/core/hypcore.py
+++ core/core/hypcore.py
@@ -1,9 +1,11 @@
-import aiohttp
+# import aiohttp
 import asyncio
 import httpx
 
+client = httpx.AsyncClient()
 
-def get_hyp_api_url(区服: str, api: str, language: str = "zh-cn") -> str:
+
+def get_hyp_api_url(区服: str, api: str) -> str:
     if 区服 == '国服':
         host = "hyp-api.mihoyo.com"
     elif 区服 == '国际服':
@@ -23,69 +25,67 @@ def get_launcher_id(区服: str) -> str:
         raise Exception("不支持的启动器")
 
 
-def get_hyp_api_params(区服: str = "国服", language: str = "zh-cn") -> dict:
-    return {
-        "launcher_id": get_launcher_id(区服),
-        "language": language
-    }
-
-
-async def get_game_id(游戏: str) -> str:
-    if 游戏 == "原神":
+def get_game_id(game: str) -> str:
+    if game == "原神":
         return "1Z8W5NHUQb"
-    elif 游戏 == "绝区零":
+    elif game == "绝区零":
         return "x6znKlJ0xK"
-    elif 游戏 == "崩坏星穹铁道":
+    elif game == "崩坏星穹铁道":
         return "64kMb5iAWu"
-    elif 游戏 == "崩坏3":
+    elif game == "崩坏3":
         return "osvnlOc0S8"
     else:
         raise Exception("不支持的游戏")
 
 
+def get_hyp_api_params(区服: str = "国服", language: str = "zh-cn") -> dict:
+    return {
+        "launcher_id": get_launcher_id(区服),
+        "language": language,
+        # "game_id": get_game_id(游戏)
+    }
+
+
 # https://hyp-api.mihoyo.com/hyp/hyp-connect/api/getGameContent?launcher_id=jGHBHlcOq1&game_id=1Z8W5NHUQb&language=zh-cn
-async def 获取资讯(区服, game_id: str, language="zh-cn") -> dict:
-    url = get_hyp_api_url(区服, "getGameContent", language) + \
-        f"&game_id={game_id}"
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url) as resp:
-            return await resp.json()
+async def 获取游戏内容(区服, game: str, language: str = "zh-cn") -> dict:
+    url = get_hyp_api_url(区服, "getGameContent")
+    params = get_hyp_api_params(区服, language)
+    params["game_id"] = get_game_id(game)
+    resp = await client.get(url, params=params)
+    return resp.json()
 
 
 async def 获取全部游戏(区服, language="zh-cn"):
-    url = get_hyp_api_url(区服, "getGames", language)
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url) as resp:
-            return await resp.json()
+    url = get_hyp_api_url(区服, "getGames")
+    resp = await client.get(url, params=get_hyp_api_params(区服, language))
+    return resp.json()
 
 
 async def 获取全部游戏基本信息(区服, language="zh-cn") -> dict:
-    url = get_hyp_api_url(区服, "getAllGameBasicInfo", language)
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url) as resp:
-            return await resp.json()
+    url = get_hyp_api_url(区服, "getAllGameBasicInfo")
+    resp = await client.get(url, params=get_hyp_api_params(区服, language))
+    return resp.json()
 
 
-async def 获取游戏(区服, game_id: str, language="zh-cn") -> dict:
-    url = get_hyp_api_url(区服, "getGames", language) + f"&game_id={game_id}"
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url) as resp:
-            return await resp.json()
+async def 获取游戏(区服, game: str, language="zh-cn") -> dict:
+    url = get_hyp_api_url(区服, "getGames")
+    params = get_hyp_api_params(区服, language)
+    params["game_id"] = get_game_id(game)
+    resp = await client.get(url, params=params)
 
 
-async def 获取游戏基本信息(区服, game_id: str, language="zh-cn") -> dict:
-    url = get_hyp_api_url(区服, "getAllGameBasicInfo",
-                          language) + f"&game_id={game_id}"
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url) as resp:
-            return await resp.json()
+async def 获取游戏基本信息(区服, game: str, language="zh-cn") -> dict:
+    url = get_hyp_api_url(区服, "getAllGameBasicInfo")
+    params = get_hyp_api_params(区服, language)
+    params["game_id"] = get_game_id(game)
+    resp = await client.get(url, params=params)
+    return resp.json()
 
 
 async def 获取全部游戏安装包信息(区服, language="zh-cn") -> dict:
-    url = get_hyp_api_url(区服, "getGamePackages", language)
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url) as resp:
-            return await resp.json()
+    url = get_hyp_api_url(区服, "getGamePackages")
+    resp = await client.get(url, params=get_hyp_api_params(区服, language))
+    return resp.json()
 
 
 async def 获取游戏安装包信息(区服, game_id: str, language="zh-cn") -> dict | None:
@@ -94,36 +94,33 @@ async def 获取游戏安装包信息(区服, game_id: str, language="zh-cn") ->
         if i["game"]["id"] == game_id:
             return i
 
-# 暂时不可用
+
+async def 获取多个游戏安装包信息(区服, game_ids: list[str], language="zh-cn") -> dict:
+    url = get_hyp_api_url(区服, "getGamePackages")
+    params = get_hyp_api_params(区服, language)
+    params["game_ids"] = game_ids
+    resp = await client.get(url, params=params)
+    return resp.json()
 
 
-async def 获取多个游戏安装包信息(区服, game_ids: str, language="zh-cn") -> dict:
-    url = get_hyp_api_url(区服, "getGamePackages", language) + \
-        f"&game_ids[]={game_ids}"
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url) as resp:
-            return await resp.json()
+async def 获取游戏依赖(区服, game: str, language="zh-cn") -> dict:
+    url = get_hyp_api_url(
+        区服, "getGameDeprecatedFileConfigs")
+    params = get_hyp_api_params(区服, language)
+    params["game_id"] = game
+    resp = await client.get(url, params=params)
+    return resp.json()
 
-
-async def 获取游戏依赖(区服, game_id: str, language="zh-cn") -> dict:
-    url = get_hyp_api_url(区服, "getGameDeprecatedFileConfigs",
-                          language) + f"&game_id={game_id}"
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url) as resp:
-            return await resp.json()
-
-
-# 游戏配置
-async def 获取游戏配置(区服, game_id: str, language="zh-cn") -> dict:
-    url = get_hyp_api_url(区服, "getGameConfigs", language) + \
-        f"&game_id={game_id}"
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url) as resp:
-            return await resp.json()
+async def 获取游戏配置(区服, game: str, language="zh-cn") -> dict:
+    url = get_hyp_api_url(区服, "getGameConfigs")
+    params = get_hyp_api_params(区服, language)
+    params["game_id"] = game
+    resp = await client.get(url, params=params)
+    return resp.json()
 
 
 if __name__ == "__main__":
     # print(asyncio.run(获取游戏配置("国服", "1Z8W5NHUQb")))
     # x6znKlJ0xK
     # print(asyncio.run(获取游戏配置("国服", "1Z8W5NHUQb")))
-    print(asyncio.run(获取多个游戏安装包信息("国服", "x6znKlJ0xK")))
+    print(asyncio.run(获取游戏内容("国服")))
